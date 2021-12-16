d09ac4e4d01ad68ca17f121edffa8e18239a1902
brunoguerra <brunoguerra@hotmail.com>
2021-12-16 20:45:04 -0300

    first commit

---
+++ src/test/java/br/com/bg8/cidadesapi/CidadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bg8.cidadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
